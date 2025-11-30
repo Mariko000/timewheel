abf247f5f6d03d66b3be9736474defca4feac91c
Mariko Wakabayashi <Mariko@Taisukes-MacBook-Air-2.local>
2025-11-30 18:26:08 +0900

    Fix manifest path for VitePWA

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
@@ -24,16 +25,16 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/web-app-manifest-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
+            "src": "/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            src: '/web-app-manifest-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
+            "src": "/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
           },
         ]
       },
